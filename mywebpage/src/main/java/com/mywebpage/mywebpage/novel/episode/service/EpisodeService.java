f77ef001c4e2d415cf5be2a93280e6759e1b5c34
JItzel <kikiroro1506@gmail.com>
2025-08-20 17:32:43 +0900

    service 추가
    추가

--- mywebpage/src/main/java/com/mywebpage/mywebpage/novel/episode/service/EpisodeService.java
+++ mywebpage/src/main/java/com/mywebpage/mywebpage/novel/episode/service/EpisodeService.java
@@ -2,10 +2,18 @@ package com.mywebpage.mywebpage.novel.episode.service;
 
 import com.mywebpage.mywebpage.common.ErrorMsg;
 import com.mywebpage.mywebpage.common.MapStruct;
+import com.mywebpage.mywebpage.novel.episode.dto.EpisodeDto;
+import com.mywebpage.mywebpage.novel.episode.entity.Episode;
 import com.mywebpage.mywebpage.novel.episode.repository.EpisodeRepository;
+import com.mywebpage.mywebpage.novel.series.entity.Series;
 import com.mywebpage.mywebpage.novel.series.repository.SeriesRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -14,4 +22,32 @@ public class EpisodeService {
     private final SeriesRepository seriesRepository;
     private final MapStruct mapStruct;
     private final ErrorMsg errorMsg;
+
+//    전체조회
+    public Page<EpisodeDto> getAll(Pageable pageable) {
+        Page<Episode> episodes = episodeRepository.findAll(pageable);
+        return episodes.map(data->mapStruct.toDto(data));
+    }
+
+//    추가
+    public void save(EpisodeDto dto) {
+        Episode episode = mapStruct.toEntity(dto);
+        Series series = seriesRepository.findById(dto.getSeriesId())
+                .orElseThrow(()->new RuntimeException(errorMsg.getMessage("errors.not.found")));
+        episode.setSeries(series);
+        episodeRepository.save(episode);
+    }
+
+//    update(dirty 체킹)
+    @Transactional
+    public void update(EpisodeDto dto) {
+        Episode episode = episodeRepository.findById(dto.getId())
+                .orElseThrow(()->new RuntimeException(errorMsg.getMessage("errors.not.found")));
+        mapStruct.update(dto, episode);
+    }
+
+//    삭제
+    public void delete(long id) {
+        episodeRepository.deleteById(id);
+    }
 }
--- mywebpage/src/main/java/com/mywebpage/mywebpage/novel/series/service/SeriesService.java
+++ mywebpage/src/main/java/com/mywebpage/mywebpage/novel/series/service/SeriesService.java
@@ -2,9 +2,14 @@ package com.mywebpage.mywebpage.novel.series.service;
 
 import com.mywebpage.mywebpage.common.ErrorMsg;
 import com.mywebpage.mywebpage.common.MapStruct;
+import com.mywebpage.mywebpage.novel.series.dto.SeriesDto;
+import com.mywebpage.mywebpage.novel.series.entity.Series;
 import com.mywebpage.mywebpage.novel.series.repository.SeriesRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -13,5 +18,35 @@ public class SeriesService {
     private final MapStruct mapStruct;
     private final ErrorMsg errorMsg;
 
+//  전체조회
+    public Page<SeriesDto> searchAll(Pageable pageable, String keyword) {
+        Page<Series> page = seriesRepository.searchAll(keyword, pageable);
+        return page.map(series -> mapStruct.toDto(series));
+    }
 
+//  상세조회
+    public SeriesDto findById(long id) {
+        Series series = seriesRepository.findById(id)
+                .orElseThrow(()->new RuntimeException(errorMsg.getMessage("errors.not.found")));
+        return mapStruct.toDto(series);
+    }
+
+//    추가
+    public void save(SeriesDto dto) {
+        Series series = mapStruct.toEntity(dto);
+        seriesRepository.save(series);
+    }
+
+//    update
+    @Transactional
+    public void update(SeriesDto dto) {
+        Series series = seriesRepository.findById(dto.getId())
+                .orElseThrow(()->new RuntimeException(errorMsg.getMessage("errors.not.found")));
+        mapStruct.update(dto, series);
+    }
+
+//    삭제
+    public void delete(long id) {
+        seriesRepository.deleteById(id);
+    }
 }
